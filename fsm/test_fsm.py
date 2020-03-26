4ef7b3c413ba04b674102edadce8b1ded8758476
Jiaqi Lei <leij11@mcmaster.ca>
2020-03-26 19:09:53 -0400

    Update test_fsm.py

--- fsm/test_fsm.py
+++ fsm/test_fsm.py
@@ -1,31 +1,29 @@
-import random
 from machine import Fsm
 
-states = ['A', 'C', 'B']
- # test ordered transitions
-machine = Fsm(states=states, initial='A')
-machine.add_ordered_transitions()
-machine.next_state()
-print(machine.state)
-machine.next_state()
-print(machine.state)
-
-# test custome transitions
-class Matter(object):
+class Things(object):
     pass
 
+day = Things()
+
 letter = Matter()
-states = ['A', 'B', 'C']
+states = ['School', 'Home', 'Company','Club']
 
 transitions = [
-    { 'trigger': 'first', 'source': 'A', 'dest': 'B' },
-    { 'trigger': 'second', 'source': 'A', 'dest': 'C' },
-    { 'trigger': 'third', 'source': 'B', 'dest': 'C' },
+    { 'trigger': 'Rest', 'source': 'School', 'dest': 'Home' },
+    { 'trigger': 'Study', 'source': 'Home', 'dest': 'School' },
+    { 'trigger': 'work', 'source': 'Home', 'dest': 'Company' },
+    { 'trigger': 'Play', 'source': 'Home', 'dest': 'Club' },
+    { 'trigger': 'Rest', 'source': 'Company', 'dest': 'Home' },
+    { 'trigger': 'Sleep', 'source': 'Club', 'dest': 'Home' },
+    { 'trigger': 'Finish', 'source': 'School', 'dest': 'f' },
+    { 'trigger': 'Finish', 'source': 'Company', 'dest': 'f' },
 ]
-
-# Initialize
-machine = Fsm(letter, states=states, transitions=transitions, initial='A')
-print(letter.state)
-letter.second()
-print(letter.state)
-print(machine.get_transitions())
+machine = Fsm(day, states=states, transitions=transitions, initial='Home')
+#Initial State
+print(day.state)
+#From home to school
+day.Study()
+print(day.state)
+#From school to home
+day.Rest()
+print(day.state)
