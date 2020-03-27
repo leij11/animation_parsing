4165b7281fc7946824e4aa0fe441ca121746af1b
Jiaqi Lei <leij11@mcmaster.ca>
2020-03-27 00:14:35 -0400

    Replace state.py

--- fsm/state.py
+++ fsm/state.py
@@ -9,14 +9,15 @@ Created on Fri Mar 20 14:36:18 2020
 from enum import Enum, EnumMeta
 
 class State(object):
+    """
+    A class that manages of transitions
+    that set assigned to the same trigger
+    """
     dynamic_methods = ['on_enter', 'on_exit']
 
-    def __init__(self, name, on_enter=None, on_exit=None,
-                 ignore_invalid_triggers=None):
+    def __init__(self, name, ignore_invalid_triggers=None):
         self._name = name
         self.ignore_invalid_triggers = ignore_invalid_triggers
-        self.on_enter = listify(on_enter) if on_enter else []
-        self.on_exit = listify(on_exit) if on_exit else []
 
     @property
     def name(self):
@@ -28,15 +29,6 @@ class State(object):
     @property
     def value(self):
         return self._name
-
-    def enter(self, event_data):
-        for handle in self.on_enter:
-            event_data.machine.callback(handle, event_data)
-
-    def exit(self, event_data):
-        for handle in self.on_exit:
-            event_data.machine.callback(handle, event_data)
-
 
     def __repr__(self):
         return "<%s('%s')@%s>" % (type(self).__name__, self.name, id(self))
